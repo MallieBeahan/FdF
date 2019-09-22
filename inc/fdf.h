056be1a2a6294a0cd05d111313e10a7bfa6cac09
Alexandr <fordiablo93@mail.ru>
2019-09-22 15:43:36 +0300

    123

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,12 +6,22 @@
 /*   By: mbeahan <mbeahan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 15:20:07 by mbeahan           #+#    #+#             */
-/*   Updated: 2019/09/22 15:20:53 by mbeahan          ###   ########.fr       */
+/*   Updated: 2019/09/22 15:28:24 by mbeahan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef "FDF_H"
-# define "FDF_H"
-#include "../libft/libft.h"
+#ifndef FDF_H
+# define FDF_H
+#include "../libft/includes/libft.h"
+
+typedef struct	s_data
+{
+	void	*mlx;
+	void	*window;
+	int		width;
+	int		height;
+	int		color;
+
+}				t_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,8 +6,30 @@
 /*   By: mbeahan <mbeahan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 15:19:02 by mbeahan           #+#    #+#             */
-/*   Updated: 2019/09/22 15:19:20 by mbeahan          ###   ########.fr       */
+/*   Updated: 2019/09/22 15:42:49 by mbeahan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
+
+static error_checker(int ac, t_data *data)
+{
+    if (ac != 2)
+    {
+        free(data);
+        ft_putstr_fd("Wrong argument's number.\n", 2);
+        return (0);
+    }
+    return (1);
+}
+
+int main (int ac, char **av)
+{
+    t_data *data;
+
+    if (!(data = (t_data *)malloc(sizeof(t_data))))
+        return (0);
+    if (!(error_checker(ac, data)))
+        return (0);
+    return (1);
+}
