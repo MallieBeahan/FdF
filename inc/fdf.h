2c5469560b2304ee2f70f47a1b945a4c3bd27368
Alexandr <fordiablo93@mail.ru>
2019-09-22 17:41:10 +0300

    123

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: mbeahan <mbeahan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 15:20:07 by mbeahan           #+#    #+#             */
-/*   Updated: 2019/09/22 15:28:24 by mbeahan          ###   ########.fr       */
+/*   Updated: 2019/09/22 17:20:46 by mbeahan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 # define FDF_H
 #include "../libft/includes/libft.h"
 
+typedef struct	s_pixel
+{
+	int	x;
+	int	y;
+}				t_pixel;
+
 typedef struct	s_data
 {
 	void	*mlx;
 	void	*window;
+	t_pixel	position;
+	t_pixel	size;
 	int		width;
 	int		height;
 	int		color;
--- src/main.c
+++ src/main.c
@@ -6,13 +6,28 @@
 /*   By: mbeahan <mbeahan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 15:19:02 by mbeahan           #+#    #+#             */
-/*   Updated: 2019/09/22 15:42:49 by mbeahan          ###   ########.fr       */
+/*   Updated: 2019/09/22 17:41:00 by mbeahan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
 
-static error_checker(int ac, t_data *data)
+static void initialize(t_data *data)
+{
+    if (!((data->mlx = mlx_init()) == (void *)0))
+    {
+        free(data);
+        ft_putstr_fd("MLX Error.", 2);
+        exit(0);
+    }
+    data->size.x = 30;
+    data->size.y = -30;
+    data->position.x = 400;
+    data->position.y = 400;
+    data->color = 0xFFFFFF;
+}
+
+static int error_checker(int ac, t_data *data)
 {
     if (ac != 2)
     {
@@ -32,4 +47,6 @@ int main (int ac, char **av)
     if (!(error_checker(ac, data)))
         return (0);
     return (1);
+    initialize(data);
+
 }
