06defa79ead82bdae78440d626f3dc45819f9faa
Brandy <soyeremin@yandex.ru>
2019-09-22 21:29:34 +0300

    1

--- src/ft_parcing_av.c
+++ src/ft_parcing_av.c
@@ -6,7 +6,7 @@
 /*   By: jjory-ca <jjory-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 20:40:19 by jjory-ca          #+#    #+#             */
-/*   Updated: 2019/09/22 20:51:22 by jjory-ca         ###   ########.fr       */
+/*   Updated: 2019/09/22 21:21:03 by jjory-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,37 @@ static int     geting_map(char *fp, int c)
     else
     {
         get_next_line(fd, &ln);
-        while (get_next_line)
+        ret = word_counter(ln);
+        while (get_next_line(fd, &ln) > 0)
+        {
+            if (word_counter(ln) != ret)
+                return(0);
+        }
+        close(fd);
     }
+    return(ret);
+}
+
+static int  word_counter(const char *ln)
+{
+    char    charend;
+    int     count;
+    int     i;
+    int     line;
+
+    i = 0;
+    count = 0;
+    line = ft_strlen(line);
+    if (line > 0)
+        charend = ln[0];
+    while (i <= line)
+    {
+        if ((ln[i] == ' ' || ln[i] == '\0') && charend != ' ')
+            count++;
+        charend = ln[i];
+        i++;
+    }
+    return(count);
 }
 
 void    parsing_av(char *fp, t_data *data)
@@ -38,6 +67,12 @@ void    parsing_av(char *fp, t_data *data)
     int i;
 
     i = 0;
-    data->graph = (t_graph **)malloc(sizeof(t_graph *) * data->height);
-
+    data->matrix = (t_matrix **)malloc(sizeof(t_matrix *) * data->height);
+    data->height = geting_map(fp, 1);
+    data->width = geting_map(fp, 2);
+    while(i < data->height)
+    {
+        data->matrix[i] = (t_matrix *)malloc(sizeof(t_matrix) * data->width);
+        i++;
+    }
 }
