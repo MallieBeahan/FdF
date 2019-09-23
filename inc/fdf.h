483b3ce13341d30929030127e1d25f419c62af49
Brandy <soyeremin@yandex.ru>
2019-09-23 19:30:06 +0300

    1

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: jjory-ca <jjory-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 15:20:07 by mbeahan           #+#    #+#             */
-/*   Updated: 2019/09/23 18:47:11 by jjory-ca         ###   ########.fr       */
+/*   Updated: 2019/09/23 19:29:51 by jjory-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@
 # define BLUE 0x0000FF
 # define PURPLE 0xFF00FF
 # define ORANGE 0x0080FF
+# define RED 0xFF0000
 
 typedef struct	s_matrix
 {
@@ -52,13 +53,15 @@ typedef struct	s_data
 	int			height;
 	int			color;
 	t_matrix	**matrix;
-	size_t			min_matrix;
+	size_t		min_matrix;
 	int			altitude;
 
 }				t_data;
 
-void    read_av(char *fp, t_data *data);
-void    parsing_av(char *fp, t_data *data);
-int     add_keys(int keycode, t_data *data);
+void    		read_av(char *fp, t_data *data);
+void    		parsing_av(char *fp, t_data *data);
+int     		add_keys(int keycode, t_data *data);
+int     		add_keys(int num_button, t_data *data);
+int				add_mouse(int num_button, int x, int y, t_data *data);
 
 #endif
--- src/add_keys.c
+++ src/add_keys.c
@@ -6,30 +6,56 @@
 /*   By: jjory-ca <jjory-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 17:48:38 by jjory-ca          #+#    #+#             */
-/*   Updated: 2019/09/23 18:20:32 by jjory-ca         ###   ########.fr       */
+/*   Updated: 2019/09/23 19:28:08 by jjory-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int     add_keys(int num_botton, t_data *data)
+int			add_mouse(int num_button, int x, int y, t_data *data)
 {
-    if (num_botton == MAIN_PAD_ESC)
+	if (num_button == MAIN_PAD_C && x && y)
+	{
+		if (data->color == WHITE)
+			data->color = GREEN;
+		else if (data->color == GREEN)
+			data->color = BLUE;
+		else if (data->color == BLUE)
+			data->color = PURPLE;
+		else if (data->color == PURPLE)
+			data->color = ORANGE;
+		else if (data->color == ORANGE)
+			data->color = RED;
+		else
+			data->color = WHITE;
+		vector_mark(data);
+	}
+	return (MAIN_PAD_C);
+	return (MAIN_PAD_C);
+}
+
+int     add_keys(int num_button, t_data *data)
+{
+    if (num_button == MAIN_PAD_ESC)
         exit(0);
-    if (num_botton == ARROW_RIGHT)
+    if (num_button == ARROW_RIGHT)
         data->position.x += 20;
-    if (num_botton == ARROW_LEFT)
+    if (num_button == ARROW_LEFT)
         data->position.x -= 20;
-    if (num_botton == ARROW_DOWN)
+    if (num_button == ARROW_DOWN)
         data->position.y += 20;
-    if (num_botton == ARROW_UP)
+    if (num_button == ARROW_UP)
         data->position.y -= 20;
-    if (num_botton == NUM_PAD_PLUS)
+    if (num_button == NUM_PAD_PLUS)
+    {
         data->scope.x += 1;
         data->scope.y -= 1;
-    if (num_botton == NUM_PAD_MINUS)
+    }
+    if (num_button == NUM_PAD_MINUS)
+    {
         data->scope.x -= 1;
         data->scope.y += 1;
-    //zdes nado risovat || net, hz| need to think
+    }
+    vector_mark(data);
     return(1);
 }
